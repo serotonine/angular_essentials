89c852bd26ac0e13ad03d66de4138658a08dc84b
serotonine <ks.serotonine@gmail.com>
2024-09-18 11:41:36 +0200

    Master: Change user picture.

--- src/app/user/users.ts
+++ src/app/user/users.ts
@@ -2,31 +2,31 @@ export const USERS = [
   {
     id: 'u1',
     name: 'Jasmine Washington',
-    avatar: 'user-1.jpg',
+    avatar: 'lapin.png',
   },
   {
     id: 'u2',
-    name: 'Emily Thompson',
-    avatar: 'user-2.jpg',
+    name: 'Heron',
+    avatar: 'heron.png',
   },
   {
     id: 'u3',
-    name: 'Marcus Johnson',
-    avatar: 'user-3.jpg',
+    name: 'Parrot',
+    avatar: 'perroquet.png',
   },
   {
     id: 'u4',
     name: 'David Miller',
-    avatar: 'user-4.jpg',
+    avatar: 'pitbull.png',
   },
   {
     id: 'u5',
     name: 'Priya Patel',
-    avatar: 'user-5.jpg',
+    avatar: 'mouche.png',
   },
   {
     id: 'u6',
     name: 'Arjun Singh',
-    avatar: 'user-6.jpg',
+    avatar: 'mante.png',
   },
 ];
