bd6738f07603dbcfd9206537d70472ce12e6f8ea
serotonine <ks.serotonine@gmail.com>
2024-09-19 19:25:58 +0200

    Master: Templates ng-content.

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
+  imports: [DatePipe, CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
